32741132b65a80adac7f86f8329174fc3e282294
Jasmin DeFino <126633389+jasminDe@users.noreply.github.com>
2023-10-12 22:29:11 +0000

    created header css file

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import './Header.css';
 import PersonIcon from '@mui/icons-material/Person';
 import ModeCommentIcon from '@mui/icons-material/ModeComment';
 
 function Header() {
   return (
-    <div>
+    // BEM
+    <div className='header'>
       <PersonIcon />
      <h2>I am a header</h2>
      <ModeCommentIcon />
