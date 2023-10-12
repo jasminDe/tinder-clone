38f748013c421cae744bbb6bb55a9449cff62d3d
Jasmin DeFino <126633389+jasminDe@users.noreply.github.com>
2023-10-12 22:15:20 +0000

    added person icon

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import PersonIcon from '@mui/icons-material/Person';
 
 function Header() {
   return (
     <div>
+      <PersonIcon />
      <h2>I am a header</h2>
     </div>
   )
