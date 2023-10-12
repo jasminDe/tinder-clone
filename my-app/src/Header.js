63939516382c1b9f6787cdf2d5ecab3ca368cc6f
Jasmin DeFino <126633389+jasminDe@users.noreply.github.com>
2023-10-12 22:18:54 +0000

    added messages icon

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import PersonIcon from '@mui/icons-material/Person';
+import ModeCommentIcon from '@mui/icons-material/ModeComment';
 
 function Header() {
   return (
     <div>
       <PersonIcon />
      <h2>I am a header</h2>
+     <ModeCommentIcon />
     </div>
   )
 }
