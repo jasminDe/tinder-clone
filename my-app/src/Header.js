dbf9eaff5484ca70242f220bf0379d5a30701a1d
Jasmin DeFino <126633389+jasminDe@users.noreply.github.com>
2023-10-13 00:11:02 +0000

    added button cappabilities to icons

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -2,18 +2,23 @@ import React from 'react'
 import './Header.css';
 import PersonIcon from '@mui/icons-material/Person';
 import ModeCommentIcon from '@mui/icons-material/ModeComment';
+import {IconButton}  from '@mui/material';
 
 function Header() {
   return (
     // BEM
     <div className='header'>
-      <PersonIcon className='header__icon' fontSize='large'/>
+      <IconButton>
+       <PersonIcon className='header__icon' fontSize='large'/>
+      </IconButton>
      <img 
      className='header__logo'
      src="https://i.pinimg.com/736x/d4/d8/82/d4d882dccd11187b7980ada01a465d48.jpg" 
      alt="tinder logo" 
      />
-     <ModeCommentIcon className='header__icon' fontSize='large' />
+     <IconButton>
+      <ModeCommentIcon className='header__icon' fontSize='large' />
+     </IconButton>
     </div>
   )
 }
